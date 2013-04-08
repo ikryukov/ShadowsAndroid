a19ebaf130c9227aa4e3d3b4d39db56b313b84d5
ikryukov <ilyakrukov@gmail.com>
2013-04-09 00:35:39 +0400

    daily commit

---
+++ jni/load_stuff.sh
@@ -0,0 +1,10 @@
+adb push Shadow.frag /sdcard/
+adb push Shadow.vert /sdcard/
+adb push Simple.frag /sdcard/
+adb push Simple.vert /sdcard/
+adb push plane.obj /sdcard/
+adb push quad.obj /sdcard/
+adb push scene.txt /sdcard/
+adb push teapot.obj /sdcard/
+adb push tree.obj /sdcard/
+
